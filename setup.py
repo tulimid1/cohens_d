a1d7a4385db305c63caf1f1dbbf466eb3ae797e7
Duncan Tulimieri <tulimid@udel.edu>
2021-12-21 07:55:19 -0500

    new license

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
   name = 'cohens_d',         # How you named your package folder (MyLib)
   packages = ['cohens_d'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
-  license='GPL-3.0 License',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Calculate cohen''s d for 1 sample, 2 independent samples, or 2 paired samples',   # Give a short description about your library
   author = 'Duncan Tulimieri',                   # Type in your name
   author_email = 'tulimid@udel.edu',      # Type in your E-Mail
@@ -17,7 +17,7 @@ setup(
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: GPL-3.0 License',   # Again, pick a license
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
     'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
