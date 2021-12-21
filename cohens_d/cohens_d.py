8bdcee673fe9c6ad146aea6ea73423bd5531066f
Duncan Tulimieri <tulimid@udel.edu>
2021-12-21 08:21:54 -0500

    separate 2 samples from stats and 2 samples from vectors

--- cohens_d/cohens_d.py
+++ cohens_d/cohens_d.py
@@ -13,9 +13,9 @@ def cohensd_1samp(data, mu):
     d = numpy.abs((numpy.mean(data) - mu) / numpy.std(data,ddof=1))
     return d 
 
-def cohensd_2ind(mu1, mu2, std1, std2, n1, n2):
+def cohensd_2ind_from_stats(mu1, mu2, std1, std2, n1, n2):
     '''
-    Calculate cohen's d for 2 independent samples 
+    Calculate cohen's d for 2 independent samples from statistics
 
     INPUTS:
     mu1: mean of group 1 
@@ -31,6 +31,21 @@ def cohensd_2ind(mu1, mu2, std1, std2, n1, n2):
     import numpy
     d = numpy.abs((mu1 - mu2) / numpy.sqrt(((n1 - 1)*std1**2 + (n2 - 1)*std2**2) / (n1 + n2 - 2)))
     return d 
+
+def cohensd_2ind(group1, group2):
+    '''
+    Calculate cohen's d for 2 independent samples 
+
+    INPUTS:
+    group1: group 1 data vector 
+    group2: group 2 data vector 
+
+    OUTPUTS:
+    d: absolute cohen's d 
+    '''
+    import numpy 
+    d = (numpy.mean(group1) - numpy.mean(group2)) / (numpy.sqrt((((len(group1) - 1) * numpy.var(group1, ddof=1)) + ((len(group2) - 1) * numpy.var(group2, ddof=2))) / (len(group1) + len(group2) - 2)))
+    return numpy.abs(d)
 
 def cohensd_2paired(pre, post, mu, r):
     '''
