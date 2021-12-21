56b1910cd3c34b438dbc8c8e810829cfb91e8163
Duncan Tulimieri <tulimid@udel.edu>
2021-12-21 07:51:21 -0500

    adding files for pypi

--- setup.py
+++ setup.py
@@ -1,24 +1,23 @@
 from distutils.core import setup
 setup(
-  name = 'YOURPACKAGENAME',         # How you named your package folder (MyLib)
-  packages = ['YOURPACKAGENAME'],   # Chose the same as "name"
+  name = 'cohens_d',         # How you named your package folder (MyLib)
+  packages = ['cohens_d'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
-  author = 'YOUR NAME',                   # Type in your name
-  author_email = 'your.email@domain.com',      # Type in your E-Mail
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
+  license='GPL-3.0 License',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'Calculate cohen''s d for 1 sample, 2 independent samples, or 2 paired samples',   # Give a short description about your library
+  author = 'Duncan Tulimieri',                   # Type in your name
+  author_email = 'tulimid@udel.edu',      # Type in your E-Mail
+  url = 'https://tulimid1.github.io/',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/tulimid1/cohens_d/archive/refs/tags/v_01.tar.gz',    # I explain this later on
+  keywords = ['cohens d'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+          'numpy',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'License :: OSI Approved :: GPL-3.0 License',   # Again, pick a license
     'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
